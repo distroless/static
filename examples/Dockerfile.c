4fccf1695d67efc2867f60a820c1e0d56da78699
Adrian Mouat <adrian@chainguard.dev>
2022-06-30 17:34:31 +0100

    Pull out c file.
    Using buildx didn't work.
    
    Signed-off-by: Adrian Mouat <adrian@chainguard.dev>

--- examples/Dockerfile.c
+++ examples/Dockerfile.c
@@ -1,13 +1,8 @@
-# syntax=docker/dockerfile:1.4
-
 ARG BASE=distroless.dev/static
 
 FROM gcc:12@sha256:49500bb5341dd34d66330e3c7789a3a8e12e6c5f602879fcb37dc83c147c4880 as build
 
-COPY <<EOF /hello.c
-#include <stdio.h>
-int main() { printf("Hello Distroless!"); }
-EOF
+COPY hello.c /hello.c
 RUN cc -static /hello.c -o /hello
 
 FROM $BASE

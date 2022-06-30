6ace8c8792ced9a764754abbea3f917348ab640d
Adrian Mouat <adrian@chainguard.dev>
2022-06-30 15:28:55 +0100

    Pass in image name.
    Also add highlighting.
    
    Signed-off-by: Adrian Mouat <adrian@chainguard.dev>

--- examples/Dockerfile.c
+++ examples/Dockerfile.c
@@ -1,4 +1,7 @@
 # syntax=docker/dockerfile:1.4
+
+ARG BASE=distroless.dev/static
+
 FROM gcc:12@sha256:49500bb5341dd34d66330e3c7789a3a8e12e6c5f602879fcb37dc83c147c4880 as build
 
 COPY <<EOF /hello.c
@@ -7,7 +10,7 @@ int main() { printf("Hello Distroless!"); }
 EOF
 RUN cc -static /hello.c -o /hello
 
-FROM distroless.dev/static:latest
+FROM $BASE
 
 COPY --from=build /hello /hello
 CMD ["/hello"]
--- test.sh
+++ test.sh
@@ -2,5 +2,7 @@
 
 set -o errexit -o nounset -o errtrace -o pipefail
 
-docker build --tag smoke-test --file examples/Dockerfile.c examples
+IMAGE_NAME=${IMAGE_NAME:-"distroless.dev/static"}
+
+docker build --build-arg BASE=${IMAGE_NAME} --tag smoke-test --file examples/Dockerfile.c examples
 docker run smoke-test
